80ded591b3a4177c605c3383002928a745e9d9a6
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-04 17:07:12 -0500

    add input handling header logic
    add logic for input handling header functionality

---
+++ input_handling.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "input_handling.h"
+
+using namespace std;
+
+double roundDouble( double x, int n ) {
+    stringstream ss;
+    ss << scientific << setprecision( n - 1 ) << x;
+    return stod( ss.str() );
+}
+
+void checkAnswerD(const double &input, const double &solution) {
+    if (input == solution) cout << "Correct!\n";
+    else cout << "Wrong! The answer is " << solution << '\n';
+}
+
+void checkAnswerI(const int &input, const int &solution) {
+    if (input == solution) cout << "Correct!\n";
+    else cout << "Wrong! The answer is " << solution << '\n';
+}
+
+void checkAnswerS(const string &input, const string &solution) {
+    if (input == solution) cout << "Correct!\n";
+    else cout << "Wrong! The answer is " << solution << '\n';
+}
+
+string doubleToString(double value) {
+    ostringstream oss;
+    oss << value;
+    return oss.str();
+}
