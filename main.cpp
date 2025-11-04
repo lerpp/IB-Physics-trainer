90e9ebf13cd6c99f21b138a182e26af28eea61fb
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-04 17:08:37 -0500

    removed redundant input handling definitions
    removed redundant input handling function definitions (they are now in the header)

--- main.cpp
+++ main.cpp
@@ -9,33 +9,6 @@ struct vec{
     double angle = 0.0;
 };
 
-double roundDouble( double x, int n ) {
-    stringstream ss;
-    ss << scientific << setprecision( n - 1 ) << x;
-    return stod( ss.str() );
-}
-
-string doubleToString(double value) {
-    ostringstream oss;
-    oss << value;
-    return oss.str();
-}
-
-void checkAnswerD(const double &input, const double &solution) {
-    if (input == solution) cout << "Correct!\n";
-    else cout << "Wrong! The answer is " << solution << '\n';
-}
-
-void checkAnswerI(const int &input, const int &solution) {
-    if (input == solution) cout << "Correct!\n";
-    else cout << "Wrong! The answer is " << solution << '\n';
-}
-
-void checkAnswerS(const string &input, const string &solution) {
-    if (input == solution) cout << "Correct!\n";
-    else cout << "Wrong! The answer is " << solution << '\n';
-}
-
 const vector<string> directions = {"N", "S", "E", "W"};
 
 void vdt() {
