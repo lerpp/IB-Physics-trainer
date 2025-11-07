8aaa44d58802f8a52178e2d3b6b823b9e875e086
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-07 12:14:45 -0500

    Add files via upload

---
+++ includes/projectile_motion.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include "projectile_motion.h"
+#include "input_handling.h"
+#include "add_vectors.h"
+
+struct vec{
+    double magnitude = 0.0;
+    string ns, ew;
+    double angle = 0.0;
+    string direction;
+};
+
+const vector<string> directions = {"N", "S", "E", "W"};
+
+void projectile_motion() {
+    double gravity = 9.8;
+    double initHeight = rand() % 1000 / 100.0;
+    vec initThrow = {
+        .magnitude = rand() % 1000 / 10.0,
+        .ns = "N",
+        .ew = "E",
+        .angle = rand() % 900 / 10.0
+    };
+    initThrow.direction = "[E " + doubleToString(initThrow.angle) + " N]";
+
+    vec initX = {
+        .magnitude = roundDouble(cos(degToRad(initThrow.angle)) * initThrow.magnitude, 3),
+        .ns = "N",
+        .ew = "E",
+        .angle = 0.0
+    };
+    initX.direction = "[" + initX.ew + "]";
+    vec initY = {
+        .magnitude = roundDouble(sin(degToRad(initThrow.angle)) * initThrow.magnitude, 3),
+        .ns = "N",
+        .ew = "E",
+        .angle = 90.0
+    };
+    initY.direction = "[" + initY.ns + "]";
+    vec finalY = {
+        .magnitude = sqrt(pow(initY.magnitude, 2) + 2 * gravity * initHeight),
+        .ns = "S",
+        .ew = "E",
+        .angle = 90.0,
+    };
+    finalY.direction = "[" + finalY.ns + "]";
+    vec final = sumVecs(initX, finalY);
+    double time = roundDouble((finalY.magnitude + initY.magnitude) / gravity, 3);
+    double displacementX = roundDouble(initX.magnitude * time, 3);
+    double maxHeight = roundDouble(initHeight + (pow(finalY.magnitude, 2) - pow(initY.magnitude, 2) / 2 / gravity), 3);
+    double maxHeightTime = roundDouble(initY.magnitude / gravity, 3);
+    int whichone = 1;
+    if (whichone == 0) {
+        cout << "A ball is thrown from a platform " << initHeight << "m above ground at " << initThrow.magnitude << " m/s " << initThrow.direction << "\nFind the time it takes for the ball to hit the ground\n";
+        double ans; cin >> ans;
+        checkAnswerD(ans, time);
+    }
+    if (whichone == 1) {
+        cout << "A ball is thrown from a platform " << initHeight << "m above ground at " << initThrow.magnitude << " m/s " << initThrow.direction << "\nFind the final velocity of the ball\n";
+        string ans; getline(cin, ans);
+        checkAnswerS(ans, doubleToString(final.magnitude) + " " + final.direction);
+    }
+}
---
+++ includes/projectile_motion.h
@@ -0,0 +1,8 @@
+#ifndef IB_PHYS_TOOL_PROJECTILE_MOTION_H
+#define IB_PHYS_TOOL_PROJECTILE_MOTION_H
+
+struct vec;
+
+void projectile_motion();
+
+#endif //IB_PHYS_TOOL_PROJECTILE_MOTION_H
