2ffca0a3f94a8a01b12a7e50fe46277f1cb5f154
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-09 09:50:55 -0500

    moved vec struct to header
    moved vec struct to header
    added conversion between radians and regrees

--- includes/add_vectors.h
+++ includes/add_vectors.h
@@ -1,7 +1,19 @@
 #ifndef IB_PHYS_TOOL_ADD_VECTORS_H
 #define IB_PHYS_TOOL_ADD_VECTORS_H
+#include <string>
 
-struct vec;
+using std::string;
+
+struct vec{
+    double magnitude = 0.0;
+    string ns, ew;
+    string direction;
+    double angle = 0.0;
+};
+
+double degToRad(double degrees);
+
+double radToDeg(double radians);
 
 vec sumVecs(const vec &v1, const vec &v2);
 
