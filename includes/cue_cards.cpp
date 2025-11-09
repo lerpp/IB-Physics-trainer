a9e63876377faf91fc33e7f164747ca0b83320db
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-09 11:31:19 -0500

    added new cue cards

--- includes/cue_cards.cpp
+++ includes/cue_cards.cpp
@@ -7,7 +7,11 @@ using std::vector, std::cin, std::cout, std::pair, std::string;
 
 const vector<pair<string, string>> cueCards = {
     {"What is the difference between a vector and a scalar?", "A scalar has magnitude only, no direction. Examples of scalars are speed and height\nVectors have magnitude and distance. Examples of vectors include displacement and velocity"},
-    {"What is the difference between distance and displacement?", "Distance is the total distance travelled while displacement is your position relative to your starting position\nIf I walk 10 meters forwards and 10 meters backward, my distance travelled is 20m while my displacement is 0m"}
+    {"What is the difference between distance and displacement?", "Distance is the total distance travelled while displacement is your position relative to your starting position\nIf I walk 10 meters forwards and 10 meters backward, my distance travelled is 20m while my displacement is 0m"},
+    {"What is free fall?", "Free fall is when an object experiences no forces acting on it except for gravity"},
+    {"What is terminal velocity?", "Terminal velocity is the speed an object travels at where the force of air resistance is equal to the force of gravity.\nOnce an object reaches terminal velocity, it will not accelerate or decelerate unless its cross-sectional surface area changes"},
+    {"What is a projectile?", "A projectile is an object given an initial thrust and then allowed to soar through the air under the influence of gravity alone"},
+    {"What is the definition for the range of a projectile?", "The horizontal displacement of the projectile"}
 };
 
 void pickCueCard() {
