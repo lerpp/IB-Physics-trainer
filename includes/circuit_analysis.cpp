08dd23dccc8b2eee469e0e7a53e508e193286c36
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-26 21:19:35 -0500

    added circuit analysis activity

---
+++ includes/circuit_analysis.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include "circuit_analysis.h"
+#include "input_handling.h"
+
+using std::cin, std::cout, std::string, std::vector, std::pair;
+
+vector<pair<string, vector<vector<double>>>> circuits = {
+    {
+R"(      I
+     -->        3A
+  __________/\/\/\/\/\_____________
+  |            R1                 |
+  |                               |
+ ---   -                          \
+-----                             /
+ ---   54V                     R2 \  9V
+-----  +                          /
+  |                               \
+  |                               /
+  |                               |
+  |              7Ω               |
+  |_________/\/\/\/\/\____________|
+                 R3)",
+        {
+            {24, 3, 8},
+            {9, 3, 3},
+            {21, 3, 7},
+            {54, 3, 18}
+        }
+    }
+};
+
+void circuit_analysis() {
+    SetConsoleOutputCP(CP_UTF8);
+    auto [circuit, vals] = circuits[rand() % circuits.size()];
+    cout << "Solve for the current through, voltage across, and resistance of every resistor and the source in the following circuit\nConfigure your answers in a table with columns being voltage, current, and resistance and rows being resistor 1, resistor 2, etc. and the last row being the source\n";
+    cout << circuit << '\n';
+    vector anstable(vals.size(), vector<double>(vals[0].size()));
+    for (int i = 0; i < vals.size(); i++)
+        for (int j = 0; j < vals[0].size(); j++)
+            cin >> anstable[i][j];
+    if (anstable == vals) cout << "Correct!";
+    else {
+        cout << "Wrong! Here is the correct table:\n";
+        for (const auto &i : vals) {
+            for (const auto &j : i) cout << spacePadding(j, 3);
+            cout << "\n";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ includes/circuit_analysis.h
@@ -0,0 +1,10 @@
+//
+// Created by allen on 11/26/2025.
+//
+
+#ifndef IB_PHYS_TOOL_CIRCUIT_ANALYSIS_H
+#define IB_PHYS_TOOL_CIRCUIT_ANALYSIS_H
+
+void circuit_analysis();
+
+#endif //IB_PHYS_TOOL_CIRCUIT_ANALYSIS_H
